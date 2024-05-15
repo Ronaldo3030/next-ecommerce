171f5abbec6b949b00114dd305f246bab0db4d73
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2024-05-15 16:42:13 -0300

    :sparkles: feat: customization products finalizated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import clsx from "clsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Navbar />
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(inter.className), 'bg-slate-700'}>
+        <Navbar />
+        <main className=" h-screen p-16">{children}</main>
+      </body>
     </html>
   );
 }
